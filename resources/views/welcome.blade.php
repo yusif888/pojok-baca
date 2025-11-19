0084ad2944082ef6ca8a769fc254ba46f94e2ff5
yusif888 <chusni.yusifa8@gmail.com>
2025-11-19 13:50:36 +0700

    Update custom.css and welcome.blade.php for improved styling and dynamic CSS versioning

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,4 @@
-﻿<!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="id">
 <head>
     <meta charset="utf-8">
@@ -7,7 +7,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
-    <link rel="stylesheet" href="{{ asset('css/custom.css?v=1762993010') }}">
+    <link rel="stylesheet" href="{{ asset('css/custom.css?v=' . time()) }}">
 </head>
 <body>
     <div class="main-bg">
